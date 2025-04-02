d5aa38443a003d5763bee47cbe2d806d9b9f2398
fool <evedansh123@gmail.com>
2025-04-02 18:27:21 +0530

    behh

--- frontend/src/TabInterface.jsx
+++ frontend/src/TabInterface.jsx
@@ -519,7 +519,7 @@ const TabsContainer = () => {
           )}
 
           {activeTab === "contests" && (
-            <div className="flex flex-col">
+            <div className="flex flex-col justify-center items-center">
               <div className="flex space-x-2 overflow-x-auto pb-2 sm:pb-0 mb-7">
                 {[
                   "All",
